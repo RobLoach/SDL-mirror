91b63e66158cedcd0109ff98dab62da8d9815da6
Tom Mason <wheybags@wheybags.com>
2017-10-22 18:23:49 +0200

    disable XAUDIO2 driver on windows for now, we need windows SDK support to get this working

--- include/SDL_config_windows.h
+++ include/SDL_config_windows.h
@@ -167,6 +167,7 @@ typedef unsigned int uintptr_t;
 #define SDL_AUDIO_DRIVER_WASAPI 1
 #define SDL_AUDIO_DRIVER_DSOUND 1
 #define SDL_AUDIO_DRIVER_XAUDIO2    0
+
 #define SDL_AUDIO_DRIVER_WINMM  1
 #define SDL_AUDIO_DRIVER_DISK   1
 #define SDL_AUDIO_DRIVER_DUMMY  1
